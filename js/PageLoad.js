75b45c6453d7c9b454c2d9b500f37b13e421f22b
CalebTVanDyke <ctvandyke24@gmail.com>
2014-01-29 17:43:38 +0000

    Added ajax calls and template html files for pages
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@10 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ js/PageLoad.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+
+    changeActive = function(id){
+    	$('.active').attr('class', '');
+    	$('#' + id).parent().attr('class', 'active');
+    }
+
+	$.ajax({
+            url: 'home.html',
+            type: 'POST',
+            success: function() {}
+    }).done(function(html) {
+            $("#content").html(html);
+    });
+
+    $("#home").on('click', function(){
+    	changeActive('home')
+    	$.ajax({
+            url: 'home.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+    $("#manageUsers").on('click', function(){
+    	changeActive('manageUsers');
+    	$.ajax({
+            url: 'manageUsers.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+    $("#manageLibrary").on('click', function(){
+    	changeActive('manageLibrary');
+    	$.ajax({
+            url: 'manageLibrary.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+    $("#manageGrants").on('click', function(){
+    	changeActive('manageGrants');
+    	$.ajax({
+            url: 'manageGrants.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+    $("#manageGrants").on('click', function(){
+    	changeActive('manageGrants');
+    	$.ajax({
+            url: 'manageGrants.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+    $("#spinlist").on('click', function(){
+    	changeActive('spinlist');
+    	$.ajax({
+            url: 'spinlist.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+    $("#library").on('click', function(){
+    	changeActive('library');
+    	$.ajax({
+            url: 'library.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+    $("#profile").on('click', function(){
+    	changeActive('profile');
+    	$.ajax({
+            url: 'profile.html',
+            type: 'POST',
+            success: function() {}
+	    }).done(function(html) {
+	            $("#content").html(html);
+	    });
+    })
+})

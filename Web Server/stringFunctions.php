2da7289076666238a15a836cab0f124d87d881fa
dnhushak <dnhushak@pinnacle-recording.com>
2014-02-03 19:39:58 +0000

    Whitespace errors for the string functions?
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@30 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/stringFunctions.php
+++ Web Server/stringFunctions.php
@@ -60,12 +60,34 @@ function removeSpaces($string){
  * @return The normalized string
  */
 function normalizeChars($string){
+	
+	//Change ampersands to and
+	$string = str_replace("&","and", $string);
+	//return trim($string);
 	// The array of offending characters
+<<<<<<< HEAD:Web Server/stringFunctions.php
 	include('characterReplace.php');
+=======
+<<<<<<< HEAD
+	$replace = array ('&' => 'And','À' => 'A','Á' => 'A','Â' => 'A','Ã' => 'A','Ä' => 'A','Å' => 'A','Æ' => 'AE','Ă' => 'A','à' => 'a','á' => 'a','â' => 'a','ã' => 'a','ä' => 'a','å' => 'a','ă' => 'a','æ' => 'ae','þ' => 'b','Þ' => 'B','Ç' => 'C','ç' => 'c','È' => 'E','É' => 'E','Ê' => 'E','Ë' => 'E','è' => 'e','é' => 'e','ê' => 'e','ë' => 'e','Ğ' => 'G','ğ' => 'g','Ì' => 'I','Í' => 'I','Î' => 'I','Ï' => 'I','İ' => 'I','ı' => 'i','ì' => 'i','í' => 'i','î' => 'i','ï' => 'i','Ñ' => 'N','Ò' => 'O','Ó' => 'O','Ô' => 'O','Õ' => 'O','Ö' => 'O','Ø' => 'O','ö' => 'o','ø' => 'o','ð' => 'o','ñ' => 'n','ò' => 'o','ó' => 'o','ô' => 'o','õ' => 'o','Š' => 'S','š' => 's','Ş' => 'S','ș' => 's','Ș' => 'S','ş' => 's','ß' => 'B','ț' => 't','Ț' => 'T','Ù' => 'U','Ú' => 'U','Û' => 'U','Ü' => 'U','ù' => 'u','ú' => 'u','û' => 'u','ü' => 'u','Ý' => 'Y','¥' => 'y','ý' => 'y','ý' => 'y','ÿ' => 'y','Ž' => 'Z','ž' => 'z' );
+>>>>>>> Fixed all normalizing functions except for character normalization. Still working. Also created a test script.:stringFunctions.php
 	//Replace all accented characters with regular characters
 	$string = strtr($string, $replace);
 	// Remove punctuation
 	$string = preg_replace('/[^a-z|^A-Z|\s]|\^|\|/', '', $string);
+=======
+	$replacees = array ("'","&",",","\'",".","¿","‡","»","Ë","Ã","Ï","“","Ú","Ÿ","˘","¡","…","Õ","”","⁄","›","·","È","Ì","Û","˙","˝","·","È","Ì","Û","˙","˝","¬"," ","Œ","‘","€","‚","Í","Ó","Ù","˚","√","—","’","„","Ò","ı","ƒ","À","œ","÷","‹","ü","‰","Î","Ô","ˆ","¸","ˇ" );
+	// The array of characters to replace the offenders
+	$replacers = array ("","and","","","","a","a","e","e","i","i","o","o","u","u","a","e","i","o","u","y","a","e","i","o","u","y","a","e","i","o","u","y","a","e","i","o","u","a","e","i","o","u","a","n","o","a","n","o","a","e","i","o","u","y","a","e","i","o","u","y" );
+	// Replace
+	$string = str_replace($replacees, $replacers, $string);
+	
+	//$string = preg_replace('~&([a-z]{1,2})(acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', $string);
+	//$string = preg_replace(array('~[^0-9a-z]~i', '~-+~'), ' ', $string);
+	
+	// Remove escape characters
+	$string = stripslashes($string);
+>>>>>>> Fixed all normalizing functions except for character normalization. Still working. Also created a test script.
 	return $string;
 }
 

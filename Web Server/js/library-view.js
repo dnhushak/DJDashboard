c0e2fbf2bc90356596267f2f5c8af86cb3fe5d0e
CalebTVanDyke <ctvandyke24@gmail.com>
2014-02-05 21:42:31 +0000

    More updates to library UI
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@45 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/js/library-view.js
+++ Web Server/js/library-view.js
@@ -7,11 +7,10 @@ $(document).ready(function() {
 		$('.active-column').removeClass('active-column');
 		$(this).addClass('active-column');
 	});
-
-	$('.selection .item').on('click', function(){
+	$(document).on('click', '.selection .item', function(){
 		$(this).siblings('.active-item').removeClass('active-item');
 		$(this).addClass('active-item');
-	})
+	});
 	$(document).on('click', '.tracks .item', function(){
 		$('.tracks .active-item').removeClass('active-item');
 		$(formatClass($(this).attr('class'))).addClass('active-item');
@@ -23,9 +22,14 @@ $(document).ready(function() {
     }).done(function(data) {
             var songs = JSON.parse(data); 
             for (var i = 0; i < 25; i++) {
+            	//Add artist name and album to seletion at the tom
+            	$('#artists .selection').append('<li class="item">'+songs[i]['Artist Name']+'</li>');	
+            	$('#albums .selection').append('<li class="item">'+songs[i]['Album Name']+'</li>');
+
+            	//Add all track information into the track view
             	$('#suggested .tracks').append('<li class="item '+songs[i]['idtrack']+'">'+songs[i]['Recommended']+'</li>');	
             	$('#track .tracks').append('<li class="item '+songs[i]['idtrack']+'">'+songs[i]['Name']+'</li>');	
-            	$('#artist .tracks').append('<li class="item '+songs[i]['idtrack']+'">'+songs[i]['Artist Name']+'</li>');	
+            	$('#artist .tracks').append('<li class="item '+songs[i]['idtrack']+'">'+songs[i]['Artist Name']+'</li>');
             	$('#album .tracks').append('<li class="item '+songs[i]['idtrack']+'">'+songs[i]['Album Name']+'</li>');	
             	$('#genre .tracks').append('<li class="item '+songs[i]['idtrack']+'">'+'GENRE'+'</li>');	
             	$('#play-count .tracks').append('<li class="item '+songs[i]['idtrack']+'">'+songs[i]['PlayCount']+'</li>');	

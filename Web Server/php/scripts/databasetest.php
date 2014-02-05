0365f5f1c61f8daf58438c214c796aacfa153574
rmcn96 <rmcn96@gmail.com>
2014-02-05 05:32:47 +0000

    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@39 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/php/scripts/databasetest.php
+++ Web Server/php/scripts/databasetest.php
@@ -1,30 +1,12 @@
 <?php
 include '../library/LibraryManager.php';
 	$conn = new LibraryManager();
-	$array = $conn->GetAllArtists(false);
-	
-	
-	
-	
+	$array = $conn->GetAllArtists(true);
+	$length = count($array);
+	for ($i = 0; $i < $length; $i++) 
+	{
+		print $array[$i]->getName() . "<br>";
+	}
 		
-		$length = count($array);
-		for ($i = 0; $i < $length; $i++) 
-		{
-			print $array[$i]->getName() . "<br>";
-		
-		}
-		
-		$albArr = $array[700]->getAlbums();
-		for($j = 0; $j < count($albArr); $j++)
-		{
-			print "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $albArr[$j]->getName() . "<br>";
-		}
-		
-		$trkArr = $albArr[0]->getTracks();
-		echo "Tracks : <br>Count: ".count($trkArr)."<br><br>";
-		
-		for($j = 0; $j < count($trkArr); $j++)
-		{
-			print "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $trkArr[$j]->getName() . "<br>";
-		}	
+
 ?>

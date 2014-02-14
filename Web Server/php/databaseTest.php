e2d6d8a8e27ad15cf7e6006a37ba280d882964ab
dnhushak <dnhushak@pinnacle-recording.com>
2014-02-14 22:30:39 +0000

    SQL Version
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@66 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/php/databaseTest.php
+++ Web Server/php/databaseTest.php
@@ -5,6 +5,7 @@ $username='u30919';
 $password='pkMDpK6Rh';
 $hostname='mysql.cs.iastate.edu';
 $db = new mysqli($hostname, $username, $password, $database);
+echo $db->client_version . " " . $db->client_info . "<br/>";
 if($db->connect_errno > 0){
 	die('Unable to connect to database [' . $db->connect_error . ']');
 }

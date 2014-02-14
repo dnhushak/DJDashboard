6a40ab28db17a839eeab78471fbf06390f609f9f
dnhushak <dnhushak@pinnacle-recording.com>
2014-02-14 19:57:16 +0000

    Added a database tester php file. Sanity checks
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@61 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Web Server/php/databaseTest.php
@@ -0,0 +1,13 @@
+<?php
+$database='db30919';
+$username='u30919';
+$password='pkMDpK6Rh';
+$hostname='mysql.cs.iastate.edu';
+$db = new mysqli($hostname, $username, $password, $database);
+if($db->connect_errno > 0){
+	die('Unable to connect to database [' . $db->connect_error . ']');
+}
+else{
+	die('Connection is active');
+}
+?>

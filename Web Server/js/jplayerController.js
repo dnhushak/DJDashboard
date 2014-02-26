dd35c24d6e2ca584e91bef46b2da20c5c91ba7ff
CalebTVanDyke <ctvandyke24@gmail.com>
2014-02-26 03:14:46 +0000

    Library now plays Polly by Nirvana
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@114 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/js/jplayerController.js
+++ Web Server/js/jplayerController.js
@@ -2,11 +2,10 @@ $(document).ready(function(){
 	$("#jquery_jplayer_1").jPlayer({
 		ready : function(){
 			$(this).jPlayer("setMedia", {
-				m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-            	oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+				m4a: "../resources/Polly.m4a"
 			});
 		},
 		swfPath : "../js/",
-		supplied : "m4a, oga" 
+		supplied : "m4a" 
 	});
 });

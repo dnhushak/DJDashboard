a6f5b31761f4bba7e45e41cab6e291b7a12213aa
rmcn96 <rmcn96@gmail.com>
2014-02-28 04:33:51 +0000

    Made revisions for future json objects - rclabou
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@130 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/php/scripts/insertPlaylist.php
+++ Web Server/php/scripts/insertPlaylist.php
@@ -11,11 +11,14 @@ try
 	$UserID = $_GET['UserID'];
 	$PlaylistName = $_GET['PlaylistName'];
 	
-	
+	//decode json, not working
 	$json = json_decode($input);  
 	echo $UserID;
 	echo $PlaylistName;
 	//var_dump($TrackArray);
+	//Make a list of track IDs, 
+	//{1, 2, 3, 4, 5}, nothing else needs to be sent
+	
 	$result = PlaylistManager::CreatePlaylist($UserID, $TrackArray, $PlaylistName);
 	echo $result;
 }

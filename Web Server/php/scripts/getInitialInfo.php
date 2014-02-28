c2063d36a31e06aceec65d954d927d3be6dbaff2
CalebTVanDyke <ctvandyke24@gmail.com>
2014-02-28 06:15:12 +0000

    Fixed some minor CSS bugs
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@134 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/php/scripts/getInitialInfo.php
+++ Web Server/php/scripts/getInitialInfo.php
@@ -1,5 +1,6 @@
 <?php
 	include('../classes/libraryBrowser.php');
+
 	$browser = new libraryBrowser();
 	$json_string = json_encode($browser->getInitialInfo(), JSON_PRETTY_PRINT);
 	echo $json_string;

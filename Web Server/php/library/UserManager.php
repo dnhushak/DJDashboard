e5fd6400d4eaa9dbec1550eff11a44b95ba3d922
dnhushak <dnhushak@pinnacle-recording.com>
2014-03-24 20:56:51 +0000

    Added User Manager Class. Moved login work into said class
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@212 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Web Server/php/library/UserManager.php
@@ -0,0 +1,91 @@
+<?php
+include_once '../sqlconnect.php';
+include_once '../publisher.php';
+include_once ("../classes/authConfig.php");
+include_once ("../classes/authUtil.php");
+
+class UserManager {
+	private $spOnAirLogin;
+	private $spOnAirLogout;
+	private $spUserLogin;
+	private $spUserLogout;
+	private $spAddUser;
+	private $spAddUserType;
+	private $spGetUserFromName;
+
+	public function __construct(){
+		$this->initialize();
+	}
+
+	/**
+	 * Build constant procedure names
+	 */
+	private function initialize(){
+		// Register failure procedure
+		register_shutdown_function("Publisher::fatalHandler");
+		
+		$this->spOnAirLogin = "OnAirLogin";
+		$this->spOnAirLogout = "OnAirLogout";
+		$this->spUserLogin = "UserLogin";
+		$this->spUserLogout = "UserLogout";
+		$this->spAddUser = "AddUser";
+		$this->spAddUserType = "AddUserType";
+		$this->spGetUserFromName = "GetUserFromName";
+	}
+
+	public function login($user, $pass){
+		
+		// Connect to the database
+		$conn = new SqlConnect();
+		
+		$results;
+		// Get all information by username
+		$results = $conn->callStoredProc($this->spGetUserFromName, array (
+				$user ));
+		
+		// If username does not exist
+		if ($results === true || $results === false) {
+			echo json_encode(array (
+					"error" => "Username and password did not match."));
+			session_unset();
+			exit();
+		}
+		
+		if (!$userinfo = mysqli_fetch_assoc($results)) {
+			// error
+		}
+		
+		// Get stored username, hashed password, and salt
+		else {
+			$user = $userinfo ['username'];
+			$hash = $userinfo ['passwordhash'];
+			$salt = $userinfo ['salt'];
+		}
+		
+		// Verify supplied username hash and salt
+		$success = authUtil::verifyPass(hashAlgo, $hash, $salt, $user, $pass);
+		
+		// Successful login, start a new session with all info
+		if ($success) {
+			if (session_status() == PHP_SESSION_NONE) {
+				session_start();
+			}
+			$_SESSION ['user'] = $userinfo ['username'];
+			$_SESSION ['userid'] = $userinfo ['iduser'];
+			$_SESSION ['usertype'] = $userinfo ['idusertype'];
+			$_SESSION ['firstname'] = $userinfo ['firstname'];
+			$_SESSION ['lastname'] = $userinfo ['lastname'];
+			
+			exit();
+		}
+		
+		// Incorrect password for username
+		else {
+			echo json_encode(array (
+					"error" => "Username and password did not match." ));
+			session_unset();
+		}
+	}
+}
+
+?>
--- Web Server/php/scripts/login.php
+++ Web Server/php/scripts/login.php
@@ -1,44 +1,8 @@
 <?php
-include_once ("../classes/authConfig.php");
-include_once ("../classes/authUtil.php");
-include_once ("../sqlconnect.php");
+include('../library/UserManager.php');
 // Get the posted user and password
 $user = $_POST ['user'];
 $pass = $_POST ['pass'];
-// Name the stored procedure we're calling
-$proc = "GetUserFromName";
-// Connect to the database
-$conn = new SqlConnect();
-$results;
-$results = $conn->callStoredProc($proc, array ( $user ));
-
-if($results === true || $results === false){
-	echo json_encode(array("error" => "Username and password did not match."));
-	session_unset();
-	exit();
-}
-
-if (!$userinfo = mysqli_fetch_assoc($results)) {
-	// error
-}
-else {
-	$user = $userinfo['username'];
-	$hash = $userinfo['passwordhash'];
-	$salt = $userinfo['salt'];
-}
-$success = authUtil::verifyPass(hashAlgo, $hash, $salt, $user, $pass);
-if ($success) {
-	if (session_status() == PHP_SESSION_NONE) {
-	    session_start();
-	}
-	$_SESSION ['user'] = $userinfo ['username'];
-	$_SESSION ['userid'] = $userinfo ['iduser'];
-	$_SESSION ['usertype'] = $userinfo ['idusertype'];
-	$_SESSION ['firstname'] = $userinfo ['firstname'];
-	$_SESSION ['lastname'] = $userinfo ['lastname'];
-	exit();
-}
-else {
-	echo json_encode(array("error" => "Username and password did not match."));
-	session_unset();
-}
+$manager = new UserManager();
+$manager->login($user, $pass);
+?>

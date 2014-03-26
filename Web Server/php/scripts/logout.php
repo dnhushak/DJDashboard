d2a828e033d5f7e844004418380151db4bd35194
rmcn96 <rmcn96@gmail.com>
2014-03-26 19:37:10 +0000

    Extended logout
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@248 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/php/scripts/logout.php
+++ Web Server/php/scripts/logout.php
@@ -1,3 +1,19 @@
 <?php
-var_dump('IN LOGOUT');
+include_once('../library/userManager.php');
+include_once('../publisher.php');
+
+//Fatal error handler for PHP
+register_shutdown_function( "Publisher::fatalHandler" );
+try {
+	if($_SESSION['sessionid'] != null)
+	{
+		UserManager::endSession($_SESSION['sessionid']);
+	}
+}
+catch (Exception $e)
+{
+	Publisher::publishException($e->getTraceAsString(), $e->getMessage(), $UserID);
+	return false;
+}
+
 ?>

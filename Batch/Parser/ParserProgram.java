27b249ff8a787bea084d2ea8108a01b037e4ea70
acole6 <acole6@iastate.edu>
2014-03-28 01:51:25 +0000

    main method for parser program
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@255 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Batch/Parser/ParserProgram.java
@@ -0,0 +1,55 @@
+package Parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import libraryManager.Library;
+
+public class ParserProgram 
+{
+	public static void main(String[] args) 
+	{
+		String path = "C:\\iTunes Library.xml";	
+		if(args.length == 1)
+		{
+			path = args[0];
+		}
+		
+	
+		File src = new File(path);
+		File directory = new File(src.getParent() + "DupLibrary");
+		File dest = new File(directory.getAbsolutePath() + "\\" + src.getName());
+		try 
+		{
+			if(!directory.exists())
+			{
+				directory.mkdir();
+			}
+			
+			
+			if(dest.exists())
+			{
+				dest.delete();
+			}
+			
+			Files.copy(src.toPath(), dest.toPath());
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		/*Library library = new Library();
+		System.out.println("Creating Library...");
+		library.createFromITunesDB(dest.getAbsolutePath());
+		System.out.println("Library created\nAdding to the database...");
+    	library.addAllToDB();*/
+    	
+    	if(directory.exists() && dest.exists())
+    	{
+    		System.out.println(dest.delete());
+    		System.out.println(directory.delete());
+    	}
+	}
+}

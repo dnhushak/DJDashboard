b351aa7cf8705df22338b65236b26902e9c1f039
rmcn96 <rmcn96@gmail.com>
2014-04-04 20:28:40 +0000

    Added recently played to home
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@290 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/js/scripts/home.js
+++ Web Server/js/scripts/home.js
@@ -3,7 +3,6 @@
  */
 
 $('document').ready(function(){
-	console.log('doc ready');
 	$.ajax({
         url: '../php/scripts/getDJs.php',
         type: 'GET',
@@ -18,10 +17,37 @@ $('document').ready(function(){
             return;
         }
         var djHTML = "";
-        for(var i = 0; i < initial.length; i++){
+        //Limit to 25 lines
+        var limit = initial.length;
+        if(limit > 25) limit = 25;
+        for(var i = 0; i < limit; i++){
            djHTML += '<li id="'+initial[i]['UserID']+'">'+initial[i]['FirstName']+' '+initial[i]['LastName']+'</li>';
         }
-        console.log(initial);
         $('.djList').html(djHTML);
     });
+	
+	
+	var getRecentlyPlayed = function(){
+        $.ajax({
+            type: "GET",
+            url: "../php/scripts/getRecentlyPlayed.php"
+        }).done(function(data){
+            var tracks;
+            try{
+                tracks = JSON.parse(data);
+            }catch(e){
+                console.log(data);
+                return;
+            }
+          //Limit to 25 lines
+            var limit = tracks.length;
+            if(limit > 25) limit = 25;
+            for(var i = 0; i < limit; i++){
+                $('.recentList').append('<li id="'+tracks[i]['TrackID']+'">' + tracks[i]['TrackName'] + ' - ' + tracks[i]['Artist'] +'</li>')
+            }
+        });
+    }
+	
+	getRecentlyPlayed();
 });
+

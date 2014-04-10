41726016765fb34d3ff157bfc82d9b18d1c62c25
CalebTVanDyke <ctvandyke24@gmail.com>
2014-04-10 18:37:18 +0000

    Added displaying admin privliages if user has admin priviages
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@295 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/webpages/djdashboard.php
+++ Web Server/webpages/djdashboard.php
@@ -49,6 +49,33 @@
 			<li class="cursor"><a id="library">Library</a></li>
 			<li class="cursor"><a id="on-air">On-Air</a></li>
 			<li class="cursor"><a id="profile">Profile</a></li>
+			<?php
+				if (session_status() == PHP_SESSION_NONE) {
+				    session_start();
+				}
+				if(isset($_SESSION['ppsamanage']) || isset($_SESSION['pgrantedit']) || isset($_SESSION['pmanageusers'])
+						|| isset($_SESSION['ppermissionedit']) || isset($_SESSION['peditusertype'])){
+
+					$htmlStr = '<li class="cursor dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Admin<span class="caret"></span></a><ul class="dropdown-menu">';
+			    	if(isset($_SESSION['ppsamanage'])){
+						$htmlStr .= '<li><a href="">Manage PSAs</a></li>';
+					}
+					if(isset($_SESSION['pgrantedit'])){
+						$htmlStr .= '<li><a href="">Manage Grants</a></li>';
+					}
+					if(isset($_SESSION['pmanageusers'])){
+						$htmlStr .= '<li><a href="">Manage Users</a></li>';
+					}
+					if(isset($_SESSION['ppermissionedit'])){
+						$htmlStr .= '<li><a href="">Manage Permissions</a></li>';
+					}
+					if(isset($_SESSION['peditusertype'])){
+						$htmlStr .= '<li><a href="">Manage User Types</a></li>';
+					}
+					$htmlStr .= '</ul></li>';
+					echo $htmlStr;
+				}
+			?>
 		</ul>
 		<div id="content"></div>
 		<div id="footer">

801fabf6f9d6126630362af89dd36c6e683c3ddd
dnhushak <dnhushak@pinnacle-recording.com>
2014-04-14 00:36:05 +0000

    ?
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@305 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/php/scripts/playTrackByID.php
+++ Web Server/php/scripts/playTrackByID.php
@@ -6,24 +6,26 @@ include_once ('../publisher.php');
 register_shutdown_function("Publisher::fatalHandler");
 try {
 	if (session_status() == PHP_SESSION_NONE) {
-	    session_start();
+		session_start();
 	}
 	$manager = new LibraryManager();
-
-	if(!($manager->IsOnAir())){
-		echo json_encode(array("errorMessage" => "Another user has gone on air and you have been kicked off",
-								"error" => "Validation Error"));
+	
+	if (!($manager->IsOnAir())) {
+		echo json_encode(array (
+				"errorMessage" => "Another user has gone on air and you have been kicked off",
+				"error" => "Validation Error" ));
 		die();
 	}
 	$TrackID = $_GET ['TrackID'];
 	$UserID = $_SESSION ['userid'];
-	$OnAirSessionID = 0;//$_SESSION['onairid'];
+	$OnAirSessionID = 0; // $_SESSION['onairid'];
 	$result = $manager->PlayTrack($UserID, $TrackID, $OnAirSessionID);
-	echo json_encode(array("PlayID" => $result)); //Echo the final ID to send back to frontend
+	echo json_encode(array (
+			"PlayID" => $result )); // Echo the final ID to send back to frontend
 }
 catch (Exception $e) {
 	Publisher::publishException($e->getTraceAsString(), $e->getMessage(), $UserID);
 	return false;
-}	
+}
 
 ?>

676a17d276bf638f9ee626734ba47a71378b87ac
rmcn96 <rmcn96@gmail.com>
2014-04-23 04:33:11 +0000

    Exception thrower
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@355 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Web Server/tests/exceptionThrower.php
@@ -0,0 +1,17 @@
+<?php
+include_once('../php/publisher.php');
+// Fatal error handler for PHP
+register_shutdown_function("Publisher::fatalHandler");
+try {
+	echo 'This is an Exception thrower.  This script throws an exception to be caught as most should be.';
+	echo 'Exception is thrown right after this line.';
+	throw new Exception('Test Exception from ExceptionThrower.php');
+}
+catch (Exception $e) {
+	echo 'Exception Caught';
+	Publisher::publishException($e->getTraceAsString(), $e->getMessage(), $UserID);
+	echo 'Exception Published';
+	return false;
+}
+
+?>

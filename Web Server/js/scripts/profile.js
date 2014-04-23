a9c071b5199779f8b7dfe17b85560572b40dd665
rmcn96 <rmcn96@gmail.com>
2014-04-23 19:13:14 +0000

    Profile work
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@359 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/js/scripts/profile.js
+++ Web Server/js/scripts/profile.js
@@ -2,72 +2,67 @@
  * Manages the profiles
  */
 
+$('document').ready(
+		function() {
 
-$('document').ready(function(){
-	
-	
-	var getCurrentUsersProfile = function(){
-        $.ajax({
-            type: "GET",
-            url: "../php/scripts/getCurrentUserProfile.php"
-        }).done(function(data){
-            var profile;
-            try{
-                profile = JSON.parse(data);
-            }catch(e){
-                console.log(data);
-                return;
-            }
-            var djNickName = profile['NickName'];
-            var djMotto = profile['Motto'];
-            var djBio = profile['Bio'];
-            for (var i = 0; i < profile.length; i++) {
-            
-            }
-            
-            
-            
-        });
-    }
-	
-	var getFivePlaylists = function(){
-		$.ajax({
-            type: "GET",
-            url: "../php/scripts/GetProfilePlaylists.php"
-        }).done(function(data){
-            var profile;
-            try{
-                profile = JSON.parse(data);
-            }catch(e){
-                console.log(data);
-                return;
-            }
-            var trackName = profile['TrackName'];
-            var djMotto = profile['Motto'];
-            var djBio = profile['Bio'];
-            
-            if(djNickName != ""){
-                $('#djTitle').append(djNickName + " <small> " + djMotto + "</small>");
-            }
-            if(djBio != ""){
-            	$('#bio').append(djBio);
-            }
-            
-            
-        });
-		
-		/**
-		<div class="panel-body">
-		<ul>
-			<li><a href="#">Song One</a></li>
-			<li><a href="#">Song Two</a></li>
-			<li><a href="#">Song Three</a></li>
-			<li><a href="#">Song Four</a></li>
-			<li><a href="#">Song Five</a></li>
-		</ul>
-		</div>**/
-	}
-	
-	getCurrentUsersProfile();
-});
+			var getCurrentUsersProfile = function() {
+				$.ajax({
+					type : "GET",
+					url : "../php/scripts/getCurrentUserProfile.php"
+				}).done(function(data) {
+					var profile;
+					try {
+						profile = JSON.parse(data);
+					} catch (e) {
+						console.log(data);
+						return;
+					}
+					var djNickName = profile['NickName'];
+					var djMotto = profile['Motto'];
+					var djBio = profile['Bio'];
+					for ( var i = 0; i < profile.length; i++) {
 
+					}
+
+				});
+			}
+
+			var getFivePlaylists = function() {
+				$.ajax({
+					type : "GET",
+					url : "../php/scripts/GetProfilePlaylists.php"
+				}).done(
+						function(data) {
+							var profile;
+							try {
+								playlists = JSON.parse(data);
+							} catch (e) {
+								console.log(data);
+								return;
+							}
+							for(var i = 0; i < playlists.length; i++){
+							var trackName = playlists[i]['TrackName'];
+							console.log(playlists[i]['Artist']);
+							var artistName = playlists[i]['Artist']['ArtistName'];
+							}
+							var playlistHTML = '';
+							
+
+						});
+
+				/***************************************************************
+				 * <div class="panel-body">
+				 * <ul>
+				 * <li><a href="#">Song One</a></li>
+				 * <li><a href="#">Song Two</a></li>
+				 * <li><a href="#">Song Three</a></li>
+				 * <li><a href="#">Song Four</a></li>
+				 * <li><a href="#">Song Five</a></li>
+				 * </ul>
+				 * </div>
+				 **************************************************************/
+			}
+
+			getCurrentUsersProfile();
+			getFivePlaylists();
+		});

b233744887a525650309318e2634c86a55bee541
rmcn96 <rmcn96@gmail.com>
2014-04-23 19:51:31 +0000

    profile work
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@371 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Web Server/js/scripts/profile.js
+++ Web Server/js/scripts/profile.js
@@ -48,7 +48,7 @@ $('document').ready(function() {
 			for ( var i = 0; i < playlists.length; i++) {
 				//Create playlist drop down
 				var playlistName = playlists[i]['PlaylistName'];
-				var playlistID = playlists[i]['PlaylistID'];'
+				var playlistID = playlists[i]['PlaylistID'];
 				console.log(playlistName);
 				panelHTML = panelHTML + '<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#playlist' + playlistID + '">' + playlistName + '</a></h4></div><div id="playlist' + playlistID + '" class="panel-collapse collapse"></div></div>';
 				panelHTML = panelHTML + '<div class="panel-body"><ul>';

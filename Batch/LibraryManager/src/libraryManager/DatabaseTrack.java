1f1debd173fbe5ee113c533b90be0b0d39eb653f
acole6 <acole6@iastate.edu>
2014-04-28 13:25:54 +0000

    Added DatabaseTrack class to store data that represents what the track table looks like in the database
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@392 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Batch/LibraryManager/src/libraryManager/DatabaseTrack.java
@@ -0,0 +1,100 @@
+package libraryManager;
+
+public class DatabaseTrack 
+{
+	int idalbum;
+	int idartist;
+	String name;
+	boolean FCC;
+	boolean Recommened;
+	int ITLID;
+	String Path;
+	Integer idPrimaryGenre;
+	Integer idSecondaryGenre;
+	Integer idsubsonic;
+	
+	DatabaseTrack(int idalbum, int idartist, String name, boolean FCC, boolean Recommened, int ITLID, String Path, Integer idPrimaryGenre, Integer idSecondaryGenre, Integer idsubsonic)
+	{
+		this.idalbum = idalbum;
+		this.idartist = idartist;
+		this.name = name;
+		this.FCC = FCC;
+		this.Recommened = Recommened;
+		this.ITLID = ITLID;
+		this.Path = Path;
+		this.idPrimaryGenre = idPrimaryGenre;
+		this.idSecondaryGenre = idSecondaryGenre;
+		this.idsubsonic = idsubsonic;
+	}
+	
+	public int get_idalbum()
+	{
+		return idalbum;
+	}
+	
+	public int get_idartist()
+	{
+		return idalbum;
+	}
+	
+	public String get_name()
+	{
+		return name;
+	}
+	
+	public boolean get_FCC()
+	{
+		return FCC;
+	}
+	
+	public boolean get_Recommended()
+	{
+		return Recommened;
+	}
+	
+	public int get_ITLID()
+	{
+		return ITLID;
+	}
+	
+	public String get_Path()
+	{
+		return Path;
+	}
+	
+	public Integer get_idPrimaryGenre()
+	{
+		return idPrimaryGenre;
+	}
+	
+	public Integer get_idSecondaryGenre()
+	{
+		return idSecondaryGenre;
+	}
+	
+	public Integer get_idsubsonic()
+	{
+		return idsubsonic;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj == null) { return false; }
+		if(!(obj instanceof DatabaseTrack)) { return false; }
+			
+		DatabaseTrack other = (DatabaseTrack) obj;
+		boolean result = this.idalbum == other.idalbum &&
+					this.idartist == other.idartist &&
+					this.name.equals(other.name) &&
+					this.FCC == other.FCC &&
+					this.Recommened == other.Recommened &&
+					this.ITLID == other.ITLID &&
+					this.Path.equals(other.Path);
+		
+		result = this.idPrimaryGenre == null ? this.idPrimaryGenre == other.idPrimaryGenre && result : this.idPrimaryGenre.equals(other.idPrimaryGenre) && result;
+		result = this.idSecondaryGenre == null ? this.idSecondaryGenre == other.idSecondaryGenre && result : this.idSecondaryGenre.equals(other.idSecondaryGenre) && result;
+		result = this.idsubsonic == null ? this.idsubsonic == other.idsubsonic && result : this.idsubsonic.equals(other.idsubsonic) && result;
+		return result;
+	}
+}

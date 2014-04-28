42143538b5b188eca7be11712e2731cf6d4504dc
acole6 <acole6@iastate.edu>
2014-04-28 13:16:09 +0000

    cleaned up the main of the ParserProgram
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@388 817968c2-256c-45f1-8ff6-e7619683c4bf

--- Batch/Parser/ParserProgram.java
+++ Batch/Parser/ParserProgram.java
@@ -18,14 +18,14 @@ public class ParserProgram
 {
 	public static void main(String[] args) 
 	{
-		String path = args.length == 1 ? args[0] : ITunesParser.LIBRARY_FILE_PATH;		
+		String path = args.length == 1 ? args[0] : ITunesParser.LIBRARY_FILE_PATH;	
 		File src = new File(path);
 		File directory = new File(src.getParent() + "DupLibrary");
 		File dest = new File(directory.getAbsolutePath() + "\\" + src.getName());
 		try 
 		{
-			if(!directory.exists()) { directory.mkdir(); }
-			if(dest.exists()) { dest.delete(); }
+			if(!directory.exists()) directory.mkdir();
+			if(dest.exists()) dest.delete();
 			Files.copy(src.toPath(), dest.toPath());
 		} 
 		catch (IOException e) 
@@ -35,9 +35,9 @@ public class ParserProgram
 		
 		Library library = new Library();
 		library.createFromITunesDB(dest.getAbsolutePath());
-    	library.addAllToDB();
+		library.addAllToDB();
     	
-    	if(dest.exists()) { dest.delete(); }
-    	if(directory.exists()) { directory.delete(); }
+    	if(dest.exists()) dest.delete();
+    	if(directory.exists()) directory.delete();
 	}
 }

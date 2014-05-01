cc6d3bf0fcafaeef1cda2ec95d44f1ab2a9d44e0
dnhushak <dnhushak@pinnacle-recording.com>
2014-05-01 22:52:01 +0000

    Fixed things.... again
    git-svn-id: https://repo.cs.iastate.edu/coms309/19/trunk@420 817968c2-256c-45f1-8ff6-e7619683c4bf

---
+++ Web Server/php/scripts/getTracksByChunkNew.php
@@ -0,0 +1,27 @@
+<?php
+
+	include('../library/libraryManager.php');
+	$manager = new libraryManager();
+
+	$lastTrack;
+	if(isset($_GET['LastTrack'])){
+		$lastTrack = $_GET['LastTrack'];
+	}else{
+		$lastTrack = 0;
+	}
+	$chunkSize;
+	if(isset($_GET['ChunkSize'])){
+		$chunkSize = $_GET['ChunkSize'];
+	}else{
+		$chunkSize = 100;
+	}
+// 	echo "Last Track: ". $lastTrack . "<br/>";
+// 	echo "Chunk Size: ". $chunkSize . "<br/>";
+	
+
+// 	var_dump($browser->getTrackChunk(array($lastTrack)));
+	$json_string = json_encode($manager->getTrackChunks($lastTrack, $chunkSize), JSON_PRETTY_PRINT);
+
+	echo $json_string;
+
+?>

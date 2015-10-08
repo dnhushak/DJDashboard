ea91308dfc4c64f1d6325bc98105d44ff59f99e2
dnhushak <dnhushak@pinnacle-recording.com>
2015-10-07 21:39:33 -0500

    Merging all grant/psa functions into unified 'reader' functionality; deleting excess scripts and pages

--- www/js/scripts/readerManager.js
+++ www/js/scripts/readerManager.js
@@ -1,9 +1,44 @@
-var arr;
+var readerArr = {};
+var readerTypeArr = {};
 var activeID;
 
 $('document').ready(function() {
-//
+	var getReaderTypes = function(){
+		$.ajax({
+			type : "POST",
+			data:{
+				'command' : 'getAllReaderTypes'
+			},
+			url : "../php/execute.php"
+		})
+		.done(function(data){
+			try{
+				readerTypes = JSON.parse(data);
+			}
+			catch(e){
+				console.log(data);
+				return;
+			}
+			console.log(readerTypes[0]['description']);
+	
+			$('.readerTypes').html('');
+			for (var i = 0; i< readerTypes.length; i++){
+				
+				var id = readerTypes[i]['id'];
+				readerTypeArr[id] = {};
+				readerTypeArr[id] = readerTypes[i];
+				// Assemble the HTML Table of all of the readers based on type
+				
+				// Add each reader type to the dropdown menu
+				var psaHTML = '<li><a href=#>' + readerTypeArr[id]['name'] + '<\a><\li>';
+				$('.readerTypes').append(psaHTML);
+			}
+		});
+	};
+	
+	
 	var getReaders = function() {
+		
 		// Call getAllReaders proc to get the list of every reader
 		$.ajax({
 			type : "POST",
@@ -15,10 +50,8 @@ $('document').ready(function() {
 		
 		//After getting the data, parse out the JSON 
 		.done(function(data) {
-			var profile;
 			try {
 				readers = JSON.parse(data);
-				console.log(readers);
 			} catch (e) {
 				console.log(data);
 				return;
@@ -34,43 +67,48 @@ $('document').ready(function() {
 				var title = readers[i]['readerTitle'];
 				var readsRemaining = readers[i]['readsRemaining'];
 				var organization = readers[i]['organization'];
-				var usertitle = readers[i]['Usertitle'];
-				var userID = readers[i]['UserID'];
-				var lasttitle = readers[i]['Lasttitle'];
-				var isActive = readers[i]['IsActive'];
+				var isActive = readers[i]['isActive'];
 
+				// Dump the reader readerArrays into the global readerArray, associated by id
+				readerArr[id] = {};
+				readerArr[id] = readers[i];
+				
+				// Assemble the HTML Table of all of the readers based on type
+				
+				// The starting HTML
 				var psaHTML = '';
+				
+				// Dull the background if the item is inactive
 				if (isActive == "0") {
 					psaHTML += '<tr class="' + id
 							+ '" bgcolor="#D6D6D6">';
 				} else {
 					psaHTML += '<tr class="' + id + '">';
 				}
-
+				
+				// Reader Title
 				psaHTML += '<td>' + title + '</td>';
+				
+				// Reader Organization
 				psaHTML += '<td>' + organization + '</td>';
+				
+				// Number of reads remaining
 				psaHTML += '<td>' + readsRemaining + '</td>';
-				psaHTML += '<td><button type="button" class="btn btn-primary btn-sm" id="editButton" value="'+ i +'" onClick="editButton(this.id)" value="'
+				
+				// Edit button. The value="" is what is passed on to the edit function, which is the id of the reader.
+				psaHTML += '<td><button type="button" class="btn btn-primary btn-sm" id="editButton" value="'+ id +'" onClick="editButton(this.id)" value="'
 						+ readers.length + '">Edit</button></td>';
 				psaHTML += '</td>';
 				$('.readers').append(psaHTML);
 
-				console.log(readers[1]['title']);
 			}
-
-			arr = readers;
-
-			console.log(arr[1]['title']);
-
 		});
 	};
 	$(document).on('click', '#addButton', function(){
 		addPSA();
 	});
 	$(document).on('click', '#editButton', function(evt){
-    	console.log('clicked');
-//        editButton($(this).val());
-    	editButton(1);
+        editButton($(this).val());
         evt.stopPropagation();
         evt.preventDefault();
     });
@@ -82,64 +120,48 @@ $('document').ready(function() {
 	});
 
 	function editButton(id) {
-		// Call getAllReaders proc to get the list of every reader
-		$.ajax({
-			type : "POST",
-			data:{
-				'command' : 'getReaderByID',
-				'id' : 1
-			},
-			url : "../php/execute.php"
-		})
-		
-		.done(function(data) {
-			try {
-				readers = JSON.parse(data);
-			} catch (e) {
-				console.log(data);
-				return;
-			}
-		 arr[id]['title'] = readers['title']; 
-		 arr[id]['readerText'] = readers['readerText'];
-		 arr[id]['organization'] = readers['organization'];
-		 arr[id]['readsRemaining']=readers['readsRemaining'];
+		// Load the data into the editor window and display it
 		loaddata(id);
-		//ajax complete, load the data
-		});
 	}
 
+	/**
+	 * Needs fix
+	 */
 	function addPSA(){
-		activeID = arr.length;
-		arr[activeID] = new Array();
-		arr[activeID]['id'] = "0"; //Database will check for 0
-		arr[activeID]['title'] = "";
-		arr[activeID]['CreateDate'] = "";
-		arr[activeID]['readsRemaining'] = "";
-		arr[activeID]['MaxreadsRemaining'] = "";
-		arr[activeID]['organization'] = "";
-		arr[activeID]['readerText'] = "";
-		arr[activeID]['IsActive'] = "0";
+		activeID = readerArr.length;
+		readerArr[activeID] = new readerArray();
+		readerArr[activeID]['id'] = "0"; //Database will check for 0
+		readerArr[activeID]['title'] = "";
+		readerArr[activeID]['CreateDate'] = "";
+		readerArr[activeID]['readsRemaining'] = "";
+		readerArr[activeID]['MaxreadsRemaining'] = "";
+		readerArr[activeID]['organization'] = "";
+		readerArr[activeID]['readerText'] = "";
+		readerArr[activeID]['IsActive'] = "0";
 		loaddata(activeID);
 	}
 
-	function savePSA(){
-		arr[activeID]['readerText'] = document.getElementById('input-readerText').value;
-		arr[activeID]['organization'] = document.getElementById('input-organization').value;
-		arr[activeID]['title'] = document.getElementById('input-title').value;
+	/**
+	 * Needs fix
+	 */
+	function addReader(){
+		readerArr[activeID]['readerText'] = document.getElementById('input-text').value;
+		readerArr[activeID]['organization'] = document.getElementById('input-organization').value;
+		readerArr[activeID]['title'] = document.getElementById('input-title').value;
 		var playStr = document.getElementById('input-plays').value;
 		var slashIndex = playStr.indexOf('/');
-		arr[activeID]['readsRemaining'] = playStr.substring(0, slashIndex - 1);
-		arr[activeID]['MaxreadsRemaining']  = playStr.substring(slashIndex + 1, playStr.length);
-		console.log(arr[activeID]);
+		readerArr[activeID]['readsRemaining'] = playStr.substring(0, slashIndex - 1);
+		readerArr[activeID]['MaxreadsRemaining']  = playStr.substring(slashIndex + 1, playStr.length);
+		console.log(readerArr[activeID]);
 		$.ajax({
-			type : "GET",
+			type : "POST",
 			data : {
-				'id' : arr[activeID]['id'],
-				'readerText' : arr[activeID]['readerText'],
-				'title' : arr[activeID]['title'],
-				'organization' : arr[activeID]['organization'],
-				'readsRemaining' : arr[activeID]['readsRemaining'],
-				'MaxreadsRemaining' : arr[activeID]['MaxreadsRemaining'],
+				'id' : readerArr[activeID]['id'],
+				'readerText' : readerArr[activeID]['readerText'],
+				'title' : readerArr[activeID]['title'],
+				'organization' : readerArr[activeID]['organization'],
+				'readsRemaining' : readerArr[activeID]['readsRemaining'],
+				'MaxreadsRemaining' : readerArr[activeID]['MaxreadsRemaining'],
 				'EndDate' : '2014-06-06',
 			},
 			url : "../php/scripts/updatePSA.php"
@@ -161,12 +183,12 @@ $('document').ready(function() {
 		// $('.modal-body').html('');
 		$('#load-modal').modal('show');
 		$('.modal-header').append(
-				'<h3>PSA ID ' + arr[clicked_id]['id'] + '</h3>');
-		// $('.input-readerText').append(arr[clicked_id]['readerText']);
-		document.getElementById('input-title').value = arr[clicked_id]['title'];
-		document.getElementById('input-readerText').value = arr[clicked_id]['readerText'];
-		document.getElementById('input-organization').value = arr[clicked_id]['organization'];
-		document.getElementById('input-plays').value = arr[clicked_id]['readsRemaining'] + " / " + arr[clicked_id]['MaxreadsRemaining'];
+				'<h3>Add or Update Reader' + readerArr[clicked_id]['id'] + '</h3>');
+		// $('.input-readerText').append(readerArr[clicked_id]['readerText']);
+		document.getElementById('input-title').value = readerArr[clicked_id]['readerTitle'];
+		document.getElementById('input-text').value = readerArr[clicked_id]['readerText'];
+		document.getElementById('input-organization').value = readerArr[clicked_id]['organization'];
+		document.getElementById('input-plays').value = readerArr[clicked_id]['readsRemaining'] + " / " + readerArr[clicked_id]['MaxreadsRemaining'];
 	
 	}
 	
@@ -178,5 +200,6 @@ $('document').ready(function() {
 
 	//INITIAL LOAD
 	getReaders();
+	getReaderTypes();
 
 });

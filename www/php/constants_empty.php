1d76c12ea7473529b662e2b3530169ac5fea4fc4
dnhushak <dnhushak@pinnacle-recording.com>
2015-12-09 21:15:19 -0600

    Made an empty constants file, to be changed at installation

---
+++ www/php/constants_empty.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Fill in the information for url, username, schema (default table) and password 
+ * for your particular MySQL Database connection. Then, rename this file to "constants.php"
+ *
+ */
+const SQL_URL = 'your.server.url';
+const SQL_USERNAME = 'username';
+const SQL_SCHEMA = 'yourSQLtable';
+const SQL_PASSWORD = 'yourpassword';
+?>
